fd7dc6f9b04176bac8558eff7ba6b0f97ced26d5
Patrick Nguyen <tnguyen1213@hotmail.com>
2017-03-27 06:51:55 -0500

    Changed package name

--- src/com/ttnsystems/oca/OCAJavaSE7.java
+++ src/com/ttnsystems/oca/OCAJavaSE7.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ttnsystems;
+package com.ttnsystems.oca;
 
 /**
  *
@@ -16,6 +16,7 @@ public class OCAJavaSE7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world!");
     }
     
 }
