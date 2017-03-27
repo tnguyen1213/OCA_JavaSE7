6b93721bf5b0d2cd0bdd099d79d70cd2bc0f5e4d
Patrick Nguyen <pnguyen@ast.com>
2017-03-27 08:51:27 -0500

    added args

--- src/com/ttnsystems/oca/OCAJavaSE7.java
+++ src/com/ttnsystems/oca/OCAJavaSE7.java
@@ -17,6 +17,9 @@ public class OCAJavaSE7 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!");
+        if (args.length > 0){
+            System.out.println("Hello " + args[0]);
+        }
     }
     
 }
