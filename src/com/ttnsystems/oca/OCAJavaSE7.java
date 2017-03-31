98211f901bc9d7efec6dc00af683d107e9b2a900
Patrick Nguyen <pnguyen@ast.com>
2017-03-31 08:41:47 -0500

    Added a line of text.

--- src/com/ttnsystems/oca/OCAJavaSE7.java
+++ src/com/ttnsystems/oca/OCAJavaSE7.java
@@ -17,6 +17,7 @@ public class OCAJavaSE7 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!");
+        System.out.println("Another line.");
         if (args.length > 0){
             System.out.println("Hello " + args[0]);
         }
